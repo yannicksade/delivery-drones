090fd573e4bbd6c0fc92669372da454aae7775dc
D:\Git <yannicksade@gmail.com>
2023-03-08 13:46:24 +0100

    abstract mapper and DTO segragation

--- src/main/java/com/musala/delivery/drones/mappers/DroneMapper.java
+++ src/main/java/com/musala/delivery/drones/mappers/DroneMapper.java
@@ -1,5 +1,15 @@
 package com.musala.delivery.drones.mappers;
 
+import com.musala.delivery.drones.dto.DroneDto;
+import com.musala.delivery.drones.entities.Drone;
+
 public abstract class DroneMapper {
+
+	public abstract class MedicationMapper {
+
+		abstract Drone toEntity(DroneDto droneDto);
+
+		abstract DroneDto toDto(Drone drone);
+	}
 
 }
--- src/main/java/com/musala/delivery/drones/mappers/MedicationMapper.java
+++ src/main/java/com/musala/delivery/drones/mappers/MedicationMapper.java
@@ -1,5 +1,11 @@
 package com.musala.delivery.drones.mappers;
 
-public abstract class MedicationMapper {
+import com.musala.delivery.drones.dto.MedicationDto;
+import com.musala.delivery.drones.entities.Medication;
 
+public abstract class MedicationMapper {
+	
+	abstract Medication toEntity(MedicationDto medicationDto);
+	
+	abstract MedicationDto toDto(Medication medication);
 }
