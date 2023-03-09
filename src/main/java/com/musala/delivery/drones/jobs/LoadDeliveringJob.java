85472ba42f7c364465d3390053a28e20364713f5
D:\Git <yannicksade@gmail.com>
2023-03-10 00:29:08 +0100

    delivering code improvement

--- src/main/java/com/musala/delivery/drones/jobs/LoadDeliveringJob.java
+++ src/main/java/com/musala/delivery/drones/jobs/LoadDeliveringJob.java
@@ -28,39 +28,39 @@ public class LoadDeliveringJob {
         drones.stream().forEach(
                 drone -> {
                     HistoryDto activityHistory = activityHistoryService.getHistoriesByDrone(drone.getId(), HistoryRequestDto.builder().historyState(EStatus.DELIVERING).build())
-                            .stream().findAny().get();
+                            .stream().findAny().orElse(null);
                     log.info("Activity history found for drone id {} in DELIVERING state", drone.getId());
-
-                    switch (drone.getState()) {
-                        case LOADING -> {
-                            droneService.updateDroneStateById(drone.getId(), EStatus.LOADED);
-                            log.info("Drone with Id {} is DELIVERING", drone.getId());
-                            activityHistoryService.updateActivityState(activityHistory.getId(), EStatus.DELIVERING);
-                            log.info("Activity history identified By {} changed status from LOADED to DELIVERING", activityHistory.getId());
+                    if (activityHistory != null)
+                        switch (drone.getState()) {
+                            case LOADING -> {
+                                droneService.updateDroneStateById(drone.getId(), EStatus.LOADED);
+                                log.info("Drone with Id {} is DELIVERING", drone.getId());
+                                activityHistoryService.updateActivityState(activityHistory.getId(), EStatus.DELIVERING);
+                                log.info("Activity history identified By {} changed status from LOADED to DELIVERING", activityHistory.getId());
+                            }
+                            case LOADED -> {
+                                droneService.updateDroneStateById(drone.getId(), EStatus.DELIVERING);
+                                log.info("Drone with id {} changed state from LOADED to DELIVERING", drone.getId());
+                            }
+                            case DELIVERING -> {
+                                droneService.updateDroneStateById(drone.getId(), EStatus.DELIVERED);
+                                log.info("Drone with id {} changed state from DELIVERING to DELIVERED", drone.getId());
+                                activityHistoryService.updateActivityState(activityHistory.getId(), EStatus.DELIVERED);
+                                log.info("Activity history identified By {} changed status to DELIVERED ", activityHistory.getId());
+                            }
+                            case DELIVERED -> {
+                                droneService.updateDroneStateById(drone.getId(), EStatus.RETURNING);
+                                log.info("drone with id {} status updated from DELIVERED to RETURNING", drone.getId());
+                                drone.setMedications(null);
+                                droneService.save(drone);
+                            }
+                            case RETURNING -> {
+                                droneService.updateDroneStateById(drone.getId(), EStatus.IDLE);
+                                log.info("drone with id {} status updated from RETURNING to IDLE", drone.getId());
+                            }
+                            default -> {
+                            }
                         }
-                        case LOADED -> {
-                            droneService.updateDroneStateById(drone.getId(), EStatus.DELIVERING);
-                            log.info("Drone with id {} changed state from LOADED to DELIVERING", drone.getId());
-                        }
-                        case DELIVERING -> {
-                            droneService.updateDroneStateById(drone.getId(), EStatus.DELIVERED);
-                            log.info("Drone with id {} changed state from DELIVERING to DELIVERED", drone.getId());
-                            activityHistoryService.updateActivityState(activityHistory.getId(), EStatus.DELIVERED);
-                            log.info("Activity history identified By {} changed status to DELIVERED ", activityHistory.getId());
-                        }
-                        case DELIVERED -> {
-                            droneService.updateDroneStateById(drone.getId(), EStatus.RETURNING);
-                            log.info("drone with id {} status updated from DELIVERED to RETURNING", drone.getId());
-                            drone.setMedications(null);
-                            droneService.save(drone);
-                        }
-                        case RETURNING -> {
-                            droneService.updateDroneStateById(drone.getId(), EStatus.IDLE);
-                            log.info("drone with id {} status updated from RETURNING to IDLE", drone.getId());
-                        }
-                        default -> {
-                        }
-                    }
                 }
         );
     }
